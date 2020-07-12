20cd27bbff03d867cb82624ccdab6831f4558cf9
Ben Ord <bord@candoris.com>
2020-07-12 19:55:25 -0400

    Update main recipe query

--- app.ts
+++ app.ts
@@ -15,13 +15,24 @@ app.get('/recipes', function (req, res) {
       console.log(err);
       res.status(500).send(err);
     }
+
     connection.query(`
       SELECT 
-        * 
-      FROM recipes r 
+        r.id,
+        r.title,
+        r.prep_time,
+        r.cook_time,
+        r.servings,
+        r.description,
+        r.date_modified,
+        CONCAT('[',GROUP_CONCAT(CONCAT('{"id":',inst.id,',"sort":',inst.sort_order,',"instruction":"',inst.instruction,'"}') ORDER BY inst.sort_order ASC),']') AS instructions,
+        CONCAT('[',GROUP_CONCAT(CONCAT('{"id":',i.id,',"name":',i.name,',"amount":"',rti.amount,',"unit":"',rti.unit,'"}')),']') AS ingredients,
+      FROM recipes r
       JOIN instruction_steps inst ON r.id = inst.recipe_id
       JOIN recipe_to_ingredients rti ON r.id = rti.recipe_id
       JOIN ingredients i ON rti.ingredient_id = i.id
+      GROUP BY r.id
+      ORDER BY r.title ASC
       `, function (err, rows, fields) {
       if (err) {
         connection.release();
